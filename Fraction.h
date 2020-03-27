435273885195205e9a5ab13cb3783567377fd63c
Pellissier <dav.pellissier@gmail.com>
2020-03-27 18:00:42 +0100

    Fonctions Conversion et Simplification

--- Fraction.h
+++ Fraction.h
@@ -1,6 +1,47 @@
 #ifndef INF2_LABO4_FRACTION_H
 #define INF2_LABO4_FRACTION_H
 
+template <typename T>
+class Fraction {
+public:
+    /**
+     * Construit une fraction grâce au numerateur et au dénominateur
+     * @param numerateur
+     * @param denominateur
+     */
+    Fraction<T>(T numerateur, T denominateur);
+
+
+    /**
+     * Convertit une fraction dans un type précis
+     * @tparam typeConverti le type dans lequel convertir la fraction
+     * @return la fraction convertie
+     */
+    template <typename typeConverti>
+    typeConverti convertir () const;
+
+    /**
+     * Simplifie la fraction
+     * @return la fraction simplifiée
+     * @details La fonction divise le numérateur et le dénominateur par leur PGCD puis retourne le résultat
+     *          sous forme d'un nouvel objet
+     */
+    Fraction<T> simplifier () const;
+
+private:
+    T numerateur;
+    T denominateur;
+
+    /**
+     * Calcule le PGCD de deux nombres
+     * @param x le premier nombre
+     * @param y le deuxième nombre
+     * @return le PGCD de X et Y
+     * @details La fonction utilise l'algorithme d'Euclide vu dans le cours ASD1. La version itérative de l'algorithme
+     *          est utilisée car elle est plus efficace sur de grands nombres.
+     */
+    T pgcd (T x, T y) const;
+};
 
 #include "FractionImpl.h"
 #endif //INF2_LABO4_FRACTION_H
--- FractionImpl.h
+++ FractionImpl.h
@@ -1,4 +1,53 @@
 #ifndef INF2_LABO4_FRACTIONIMPL_H
 #define INF2_LABO4_FRACTIONIMPL_H
 
+using namespace std;
+
+template <typename T>
+Fraction<T>::Fraction(T numerateur, T denominateur) : numerateur(numerateur), denominateur(denominateur){ }
+
+template <typename T> template <typename typeConverti>
+typeConverti Fraction<T>::convertir() const {
+    long double nombreConverti = (long double)numerateur/denominateur;
+
+    return typeConverti(nombreConverti);
+}
+
+template <typename T>
+Fraction<T> Fraction<T>::simplifier() const {
+
+    // Cas triviaux: 0/X -> 0/1, 1/X -> 1/X, X/1 -> X/1
+    if(numerateur == 0) {
+        return Fraction(0, 1);
+    }
+    else if(numerateur == 1 || denominateur == 1) {
+        return this;
+    }
+
+    T div = pgcd(numerateur, denominateur);
+
+    // TODO: Ce if sert à optimiser 2 divisions par 1. Est-ce négligeable ?
+    if(div != 1) {
+        return Fraction<T>(numerateur/div, denominateur/div);
+    }
+    else {
+        return this;
+    }
+
+}
+
+template<typename T>
+T Fraction<T>::pgcd (T x, T y) const{
+    T reste = x%y;
+
+    while(reste)
+    {
+        x = y;
+        y = reste;
+        reste = x%y;
+    }
+
+    return y;
+}
+
 #endif //INF2_LABO4_FRACTIONIMPL_H
